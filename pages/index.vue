54b2be948757e81726e68b162a1ecdcce0827621
Fariz313 <77776418+Fariz313@users.noreply.github.com>
2022-02-17 00:11:11 +0700

    float

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,59 @@
 <template>
   <div>
     <div style="height: 100vh" id="map-conatiner"></div>
+    <div v-if="hover" class="legend pb-3">
+      <div class="container-fluid">
+        <div class="card bg-light p-3 px-5">
+          <div>
+            {{ prov }}
+          </div>
+          <div class="d-flex justify-content-around">
+            <div class="card bg-white p-1">
+              <div class="card-body">
+                <img
+                  style="height: 25px; width: auto"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Partai_NasDem.svg/640px-Partai_NasDem.svg.png"
+                />
+                <strong>{{ getRndInteger(10, 50) }}%</strong>
+                ({{ getRndInteger(10, 100) }} Kursi)
+              </div>
+            </div>
+            <div class="card bg-white p-1">
+              <div class="card-body">
+                <img
+                  style="height: 25px; width: auto"
+                  src="https://awsimages.detik.net.id/community/media/visual/2019/04/15/0ed3f774-4eb9-4275-b7f2-9b514f9676e9_11.jpeg?w=1200"
+                />
+                <strong>{{ getRndInteger(10, 50) }}%</strong>
+                ({{ getRndInteger(10, 100) }} Kursi)
+              </div>
+            </div>
+            <div class="card bg-white p-1">
+              <div class="card-body">
+                <img
+                  style="height: 25px; width: auto"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Logo_of_the_Democratic_Party_%28Indonesia%29.svg/1200px-Logo_of_the_Democratic_Party_%28Indonesia%29.svg.png"
+                />
+                <strong>{{ getRndInteger(10, 50) }}%</strong>
+                ({{ getRndInteger(10, 100) }} Kursi)
+              </div>
+            </div>
+            <div class="card bg-white p-1">
+              <div class="card-body">
+                <img
+                  style="height: 25px; width: auto"
+                  src="https://kilasjatim.com/wp-content/uploads/2019/01/Screenshot_2019-01-31-11-15-11-814_com.android.chrome.jpg"
+                />
+                <strong>{{ getRndInteger(10, 50) }}%</strong>
+                ({{ getRndInteger(10, 100) }} Kursi)
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <img class="top-left" height="100px" src="https://i0.wp.com/www.dprd-ponorogo.go.id/wp-content/uploads/2021/10/Partai-Nasdem-Preview.png?fit=501%2C301&ssl=1" alt="" srcset="">
+    
   </div>
 </template>
 
@@ -12,6 +65,8 @@ export default {
   data() {
     return {
       paintData: {},
+      prov: '',
+      hover: false,
     }
   },
   methods: {
@@ -30,7 +85,7 @@ export default {
         let firstSymbolId
         for (const layer of layers) {
           if (layer.type === 'symbol') {
-            console.log(layer.type);
+            console.log(layer.type)
             firstSymbolId = layer.id
             break
           }
@@ -69,15 +124,35 @@ export default {
           },
           firstSymbolId
         )
+
+        const coordinates = [
+          [95.0, 6.0],
+          [141.0, -11.0],
+        ]
+        // Create a 'LngLatBounds' with both corners at the first coordinate.
+        const bounds = new mapboxgl.LngLatBounds([95.0, 6.0], [141.0, -11.0])
+
+        // Extend the 'LngLatBounds' to include every coordinate in the bounds result.
+        for (const coord of coordinates) {
+          bounds.extend(coord)
+        }
+
+        map.fitBounds(bounds, {
+          padding: 20,
+        })
       })
       map.on('click', 'area-boundary', (e) => {
-          window.open('/regencies/'+e.features[0].properties.ID, 'newwindow');
+        this.$router.push('/regencies/' + e.features[0].properties.ID)
       })
 
       map.on('mouseenter', 'area-boundary', (e) => {
-        map.getCanvas().style.cursor = 'pointer'
+        this.prov = e.features[0].properties.Propinsi
+        this.hover = true
       })
-    
+      map.on('mouseleave', 'area-boundary', (e) => {
+        this.prov = ''
+        this.hover = false
+      })
     },
     getRndInteger(min, max) {
       return Math.floor(Math.random() * (max - min)) + min
--- pages/regencies/_id.vue
+++ pages/regencies/_id.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <div style="height: 100vh" id="map-conatiner"></div>
+    <img class="top-left" height="100px" src="https://i0.wp.com/www.dprd-ponorogo.go.id/wp-content/uploads/2021/10/Partai-Nasdem-Preview.png?fit=501%2C301&ssl=1" alt="" srcset="">
+    <nuxt-link class="top-right" to="/">
+          <img height="100%" src="http://cdn.onlinewebfonts.com/svg/img_490217.png" alt="" srcset="">
+        </nuxt-link>
   </div>
 </template>
 
