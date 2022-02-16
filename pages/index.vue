611b3e76f3ffd5569da77c16313951e7daa4e148
Fariz313 <77776418+Fariz313@users.noreply.github.com>
2022-02-16 12:11:22 +0700

    map

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div style="height:500px" id="map-conatiner">
+    </div>
+    <Tutorial />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js')
+
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiZmFyaXozMTMiLCJhIjoiY2t6cDE4aXB5MjBxMDJvbnh6cTY5dHhzciJ9.mgc1iru7ABp6eaFTEfQQ_Q'
+    // eslint-disable-next-line no-unused-vars
+    const map = new mapboxgl.Map({
+      container: 'map-conatiner',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [106.816666, -6.200000],
+      zoom:4,
+    })
+  },
+}
 </script>
